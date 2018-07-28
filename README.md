4e2d4ed79ebc16c224df3b81a185fda12e281733
Roller Davic <rollerdavic@gmail.com>
2018-07-28 07:40:55 +0700

    Create Roller

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ Specifying the `--testnet` flag however will reconfigure your Geth instance a bi
    itself one level deeper into a `testnet` subfolder (`~/.ethereum/testnet` on Linux). Note, on OSX
    and Linux this also means that attaching to a running testnet node requires the use of a custom
    endpoint since `geth attach` will try to attach to a production node endpoint by default. E.g.
-   `geth attach <datadir>/testnet/geth.ipc`. Windows users are not affected by this.
+   `geth attach <datadir>/testnet/roller.ipc`. Windows users are not affected by this.
  * Instead of connecting the main Ethereum network, the client will connect to the test network,
    which uses different P2P bootnodes, different network IDs and genesis states.
    
--- node/config_test.go
+++ node/config_test.go
@@ -73,15 +73,15 @@ func TestIPCPathResolution(t *testing.T) {
 	}{
 		{"", "", false, ""},
 		{"data", "", false, ""},
-		{"", "geth.ipc", false, filepath.Join(os.TempDir(), "geth.ipc")},
-		{"data", "geth.ipc", false, "data/geth.ipc"},
-		{"data", "./geth.ipc", false, "./geth.ipc"},
-		{"data", "/geth.ipc", false, "/geth.ipc"},
+		{"", "roller.ipc", false, filepath.Join(os.TempDir(), "roller.ipc")},
+		{"data", "roller.ipc", false, "data/roller.ipc"},
+		{"data", "./roller.ipc", false, "./roller.ipc"},
+		{"data", "/roller.ipc", false, "/roller.ipc"},
 		{"", "", true, ``},
 		{"data", "", true, ``},
-		{"", "geth.ipc", true, `\\.\pipe\geth.ipc`},
-		{"data", "geth.ipc", true, `\\.\pipe\geth.ipc`},
-		{"data", `\\.\pipe\geth.ipc`, true, `\\.\pipe\geth.ipc`},
+		{"", "roller.ipc", true, `\\.\pipe\roller.ipc`},
+		{"data", "roller.ipc", true, `\\.\pipe\roller.ipc`},
+		{"data", `\\.\pipe\roller.ipc`, true, `\\.\pipe\roller.ipc`},
 	}
 	for i, test := range tests {
 		// Only run when platform/test match
